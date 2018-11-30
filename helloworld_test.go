95443355bc9399db10491cb4d1a45a68baa7bd75
Divya Dadlani <ddadlani@pivotal.io>
2018-11-30 14:52:49 -0500

    fix test

--- helloworld_test.go
+++ helloworld_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestHandler(t *testing.T) {
 	os.Setenv("TARGET", "blah")
-	//SetTarget("test")
+	SetTarget("test")
 	target := os.Getenv("TARGET")
 	if target != "test" {
 		t.Error("target was not set correctly")
